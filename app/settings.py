d8b655d30c56d9a4d029cebaa968ae400f651937
chineduisraele <chineduisraele@gmail.com>
2022-04-15 00:45:56 +0100

    UPDATED ! allowed hosts

--- app/settings.py
+++ app/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', 'localhost:3000']
+ALLOWED_HOSTS = [env('ALLOWED_HOSTS1'), env(
+    'ALLOWED_HOSTS2'), env('ALLOWED_HOSTS3')]
 
 
 # Application definition
